72fcc84058552e5dca39f65b3464896bb24ee133
LiuHaolan <hal022@ucsd.edu>
2022-07-18 19:38:48 -0700

    precompute the rasterization

--- l5kit/l5kit/dataset/__init__.py
+++ l5kit/l5kit/dataset/__init__.py
@@ -2,5 +2,6 @@ from .agent import AgentDataset
 from .ego import BaseEgoDataset, EgoDataset, EgoDatasetVectorized
 from .select_agents import select_agents
 
+from .cached_ego import CachedEgoDataset
 
-__all__ = ["BaseEgoDataset", "EgoDataset", "EgoDatasetVectorized", "AgentDataset", "select_agents"]
+__all__ = ["BaseEgoDataset", "EgoDataset", "CachedEgoDataset", "EgoDatasetVectorized", "AgentDataset", "select_agents"]
